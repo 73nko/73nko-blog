08666b4219cb946290f6085d83902ecc61190d02
73nko <apramos89@gmail.com>
2020-11-27 15:20:36 +0100

    Logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
+export const logoQuery = graphql`
+  query LogoQUery {
+    logo: file(absolutePath: { regex: "/logo/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50, quality: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
 const Logo: React.FunctionComponent<any> = () => {
   return (
     <div className="logo">
-      <img src={'/logo.svg'} alt="73nko blog" />
+      <StaticQuery
+        query={logoQuery}
+        render={(data) => {
+          console.log({ data });
+          return (
+            <Image fixed={data.logo.childImageSharp.fixed} alt="73nko blog" />
+          );
+        }}
+      />
     </div>
   );
 };

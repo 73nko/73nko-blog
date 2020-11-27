70ff6229f9c96f23c49a0b9def72d59c145ec3ca
73nko <apramos89@gmail.com>
2020-11-27 15:08:15 +0100

    Twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,6 +89,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-twitter`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {

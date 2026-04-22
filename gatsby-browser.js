14c9247c45d9312296b5a7e9fe24dbae078def9a
73nko <apramos89@gmail.com>
2026-04-22 15:03:04 +0200

    Remove old Gatsby files and content

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-// prism theme
-import './static/prism-custom.css';
-
-// reload when serviceworker updates - https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-459105021
-export const onServiceWorkerUpdateReady = () => window.location.reload(true);

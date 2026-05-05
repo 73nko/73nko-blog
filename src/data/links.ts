f5b0ea25c805c2183051ddd0e3ed8f1bc8819a98
73nko <apramos89@gmail.com>
2026-05-05 16:02:48 +0200

    Updated cal.me link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,3 +1,3 @@
-export const CAL_BOOKING_URL = 'https://cal.com/alejandro-perez-73nko/secret';
+export const CAL_BOOKING_URL = 'https://cal.com/alejandro-perez-73nko/20min';
 
 export const CONTACT_EMAIL = 'me@alejandroperez.dev';
